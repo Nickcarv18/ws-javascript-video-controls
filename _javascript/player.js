0bc2e17dcfb91de6c2934ffe1e56c5e782de38cf
Nickcarv18 <nicolicarvalho1@hotmail.com>
2021-08-28 18:23:51 -0300

    Add botão Tela Cheia

---
+++ _javascript/player.js
@@ -0,0 +1,36 @@
+var video = document.getElementById("playerCustomizado");
+
+function telaCheia() //Tela Cheia
+{      
+
+    video.requestFullscreen();  
+
+}  
+
+function playPause()
+{
+}  
+
+function ampliar()
+{
+}  
+
+function reduzir()
+{  
+}
+ 
+function tamaNormal()
+{
+}  
+
+function volMais()
+{
+} 
+
+function VolMenos()
+{
+}  
+
+function mute()
+{      
+} 
